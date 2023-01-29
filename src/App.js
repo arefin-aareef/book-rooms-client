885e339dee07ad04b97ebfcfbf8fd5eae463b646
arefin_aareef <arefin.aareef@gmail.com>
2023-01-29 20:30:28 +0600

    register and login component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Homescreen from './screens/Homescreen';
 import Bookingscreen from './screens/Bookingscreen';
+import Registerscreen from './screens/Registerscreen';
+import Loginscreen from './screens/Loginscreen';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/home' element={<Homescreen />} />
           <Route path='/book/:roomid' element={<Bookingscreen />} />
+          <Route path='/register' element={<Registerscreen />} />
+          <Route path='/login' element={<Loginscreen />} />
         </Routes>
       </BrowserRouter>
     </div>
