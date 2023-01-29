b7b1cec2e0f26ce8802ecd4161b9e1ab1b0b0bc2
arefin_aareef <arefin.aareef@gmail.com>
2023-01-30 01:13:08 +0600

    login authentication

--- src/screens/Registerscreen.js
+++ src/screens/Registerscreen.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import Loader from "../components/Loader";
+import Error from "../components/Error";
+import Success from '../components/Success';
 
 function Registerscreen() {
   const [name, setname] =  useState('')
   const [email, setemail] = useState('')
   const [password, setpassword] = useState('')
   const [cpassword, setcpassword] = useState('')
+  const [loading, setloading] = useState(false)
+  const [error, seterror] = useState()
+  const [success, setsuccess] = useState()
 
   async function register(){
-
     if(password==cpassword)
     {
       const user={
@@ -17,13 +22,20 @@ function Registerscreen() {
         password,
         cpassword
       }
-      
       try {
-        const result = await axios.post('/api/users/register', user).data
+        setloading(true);
+        const result = await axios.post('/api/users/register', user).data;
+        setloading(false)
+        setsuccess(true)
+        setname('')
+        setemail('')
+        setpassword('')
+        setcpassword('')
       } catch (error) {
         console.log(error);
+        setloading(false)
+        seterror(true)
       }
-
     }
     else {
       alert('Password does not matched.')
@@ -32,15 +44,17 @@ function Registerscreen() {
 
   return (
     <div>
+      {loading && (<Loader />)}
+      {error && (<Error />)}
         <div className="row justify-content-center mt-5">
           <div className="col-md-5 mt-5">
+          {success && (<Success message="Registration Successful" />)}
             <div className='bs'>
               <h2 className='text-center p-3'>Register</h2>
               <input type="text" className='form-control' placeholder='Name' value={name} onChange={(e) => {setname(e.target.value)}} />
               <input type="text" className='form-control' placeholder='Email' value={email} onChange={(e) => {setemail(e.target.value)}} />
               <input type="text" className='form-control' placeholder='Password' value={password} onChange={(e) => {setpassword(e.target.value)}} />
               <input type="text" className='form-control' placeholder='Confirm Password' value={cpassword} onChange={(e) => {setcpassword(e.target.value)}} />
-
               <button className='btn btn-primary mt-3' onClick={register}>Register</button>
             </div>
           </div>
